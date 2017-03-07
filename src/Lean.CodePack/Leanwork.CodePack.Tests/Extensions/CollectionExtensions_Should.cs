c61ec1613f6ad3931fd2c56177f34bacbe937998
Jean Mallet <jean.mallet@outlook.com>
2017-03-07 19:36:28 -0300

    CollectionExtension: Incluído método para localizar e excluir um item em uma coleção.

---
+++ src/Lean.CodePack/Leanwork.CodePack.Tests/Extensions/CollectionExtensions_Should.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Leanwork.CodePack.Tests.Extensions
+{
+    [TestClass]
+    public class CollectionExtensions_Should
+    {
+        [TestMethod]
+        public void Find_and_remove_successfully()
+        {            
+            //arrange
+            var items = new List<string>() { "1", "a", "@", "2", "b", "#", "3", "c" };
+
+            //act
+            var result = items.FindAndRemove(x => x == "@");
+
+            //assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Find_and_remove_unsuccessfully()
+        {
+            //arrange
+            var items = new List<string>() { "1", "a", "@", "2", "b", "#", "3", "c" };
+
+            //act
+            var result = items.FindAndRemove(x => x == "@@");
+
+            //assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Find_and_remove_an_object_successfully()
+        {
+            //arrange
+            var id = Guid.NewGuid();
+
+            var items = new List<ColItem>()
+            {
+                new ColItem(),
+                new ColItem(),
+                new ColItem(id),
+                new ColItem(),
+                new ColItem()
+            };
+
+            //act
+            var result = items.FindAndRemove(x => x.Id == id);
+
+            //assert
+            Assert.IsTrue(result);
+        }
+
+
+        [TestMethod]
+        public void Find_and_remove_an_object_unsuccessfully()
+        {
+            //arrange
+            var id = Guid.NewGuid();
+
+            var items = new List<ColItem>()
+            {
+                new ColItem(),
+                new ColItem(),
+                new ColItem(),
+                new ColItem(),
+                new ColItem()
+            };
+
+            //act
+            var result = items.FindAndRemove(x => x.Id == id);
+
+            //assert
+            Assert.IsFalse(result);
+        }
+    }
+
+    public class ColItem
+    {
+        public Guid Id { get; set; }
+
+        public ColItem()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public ColItem(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ src/Lean.CodePack/Leanwork.CodePack/Extensions/CollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Leanwork.CodePack
+{
+    public static class CollectionExtensions
+    {
+        public static bool FindAndRemove<T>(this ICollection<T> collection, Func<T, bool> predicate)
+        {
+            var item = collection.FirstOrDefault(predicate);
+            if (item != null)
+                return collection.Remove(item);
+
+            return false;
+        }
+    }
+}
